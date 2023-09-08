364bbf1e88904ba7c3032062988499c5e0f4b9f9
Bijay Shrestha <108262068+Bijay-Shre-stha@users.noreply.github.com>
2023-09-08 22:28:45 +0545

    refactored

--- modal/blogModel.js
+++ modal/blogModel.js
@@ -9,7 +9,7 @@ module.exports = (sequelize, DataTypes) => {
       allowNull : false
     },
     description: {
-      type: DataTypes.STRING,
+      type: DataTypes.TEXT,
       allowNull:false
     },
   
