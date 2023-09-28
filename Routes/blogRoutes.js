b4ef7ca54a62195e2772699a404434d9524cb8b5
Bijay Shrestha <108262068+Bijay-Shre-stha@users.noreply.github.com>
2023-09-28 19:11:59 +0545

    fixed edit problem

--- Routes/blogRoutes.js
+++ Routes/blogRoutes.js
@@ -6,7 +6,7 @@ router.route("/").get(allBlogs);
 router.route("/create").get(renderCreateBlog).post(isAuthenticated, createBlog);
 router.route("/note/:id").get(readMore);
 router.route("/delete/:id").get(isAuthenticated,deleteBlog)
-router.route("/editBlog/:id").post(isAuthenticated,editBlog)
+router.route("/edit/:id").post(editBlog);
 router.route("/edit/:id").get(isAuthenticated,renderEditBlog)
 router.route("/myBlogs").get(isAuthenticated,renderMyBlogs)
 
